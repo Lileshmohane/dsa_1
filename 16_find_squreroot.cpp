afae3cfd73dcccc77d4efaa440932b330c91d473
Lileshmohane <87557132+Lileshmohane@users.noreply.github.com>
2022-06-15 20:30:31 +0530

    Update 16_find_squreroot.cpp

--- 16_find_squreroot.cpp
+++ 16_find_squreroot.cpp
@@ -28,11 +28,27 @@ float squ(int  n, int key){
     }
     return ans ;
 }
-int main(){
-  
+double morpre(int n,int pre ,int squ){
+    double fact  = 1;
+    double ans =  squ;
+    for (int  i = 0; i <pre; i++)
+    {
+       fact=fact/10;
+    for (double j = ans ; j*j<n; j= j+fact)
+    {
+        ans = j;
+    }
     
-   
-    cout<<"the sqare root is :"<<squ(50,3)<<endl;
-    cout<<"the sqare root is :"<<squ(10,4);
+    }
+    return ans ;
+}
+int main(){
+   int n;
+cout <<"  enter the number"<<endl;
+cin>>n;
+    int temp = squ(n);
+    cout<<"your ans is "<< morpre(n,3,temp)<<endl;
+ //   cout<<"the sqare root is :"<<squ(50,3)<<endl;
+ // cout<<"the sqare root is :"<<squ(10,4);
 return 0;
 }
